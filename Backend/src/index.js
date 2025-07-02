3c06d0f0b64393c874a158e75104e6de05575bf8
Priyanshu-Karki <karkipriyanshu330@gmail.com>
2025-07-02 19:34:18 +0530

    updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -22,9 +22,7 @@ app.use(cors({
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
+
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../Frontend/dist")))
